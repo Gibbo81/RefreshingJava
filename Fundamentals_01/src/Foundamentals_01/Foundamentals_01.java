509287253a8fb57db2ffa3b85cb4dba5dfb418e3
Giuliano Masi <masigiuliano@yahoo.it>
2016-10-08 15:33:12 +0200

    completed chapeter 01

--- Fundamentals_01/src/Foundamentals_01/Foundamentals_01.java
+++ Fundamentals_01/src/Foundamentals_01/Foundamentals_01.java
@@ -18,6 +18,23 @@ public class Foundamentals_01 {
         // TODO code application logic here
         System.out.println("Java rule");
         
+        int var1 = 1024;
+        int var2 = var1/2;
+        System.out.println("var1 contains: "+var1);
+        System.out.print("var2 contains var1/2: ");
+        System.out.println(var2);
+        
+        int var3=30, var4=100;
+        
+        Gallons g = new Gallons(10);
+        System.out.println("There are "+g.Gallons+" gallons equals to "+g.Liters+" liters");
+        
+        for(int x=0; x<6; x++)
+        {
+            System.out.println("count " +x);
+        }
+        
+        Gallons.PrintConversionTable(var3, var4);
     }
     
 }
---
+++ Fundamentals_01/src/Foundamentals_01/Gallons.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Foundamentals_01;
+
+/**
+ *
+ * @author cpsistemi
+ */
+public class Gallons {
+    public double Liters;
+    public double Gallons;
+    
+    public Gallons(double gal)
+    {
+        Gallons = gal;
+        Liters = Gallons*3.785;
+    }
+    
+    public static void PrintConversionTable(int starting, int ending)
+    {
+        int count=1;
+        for (int x=starting; x<ending; x++, count++)
+        {
+            System.out.println("Gallons "+x+" are "+x*3.785+" liters");
+            if ((count%10)==0)
+                System.out.println();
+        }
+    }
+}
