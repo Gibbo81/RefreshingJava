65db2694fb92b5d6ac4a6231ee83803e0f573643
Giuliano Masi <masigiuliano@yahoo.it>
2016-11-03 19:49:54 +0100

    queue class

--- Fundamentals_01/src/C05MoreDataTypes/Main05.java
+++ Fundamentals_01/src/C05MoreDataTypes/Main05.java
@@ -9,6 +9,17 @@ public class Main05 {
             System.out.println("Element of value: " + x);
         
         System.out.println("--------------------------------------------------");
+        System.out.println("Irregular/Rugged Array");
+        System.out.println("In java multidimensional array are created as array of array");
+        int nums[] = {1,6,3};
+        int[][] raggedarray = CrateIrregularArray(3, nums);
+        for (int x=0; x<raggedarray.length; x++)
+            for (int k=0; k<raggedarray[x].length; k++)
+                System.out.println("Rugged Array position ["+x+"]["+k+"] values: " + raggedarray[x][k]);
+        System.out.println("Rugged Array --> raggedarray.length: "+raggedarray.length + "It's the lenght of the array of array");
+        for (int x=0; x<3; x++)
+            System.out.println("raggedarray[x].length: "+raggedarray[x].length + " It's the lenght of the array number "+x);
+    
     }
     
     private static int[] CreateArray(int maxNumber)
@@ -16,6 +27,18 @@ public class Main05 {
         int[] result = new int[maxNumber];
         for (int x=0; x<maxNumber; x++)
             result[x]=x;
+        return result;
+    }
+    
+    private static int[][] CrateIrregularArray(int leng1, int[] lengs2)//ragged array
+    {
+        int[][] result = new int[leng1][];
+        for (int n=0; n<leng1; n++)
+        {
+            result[n]= new int[lengs2[n]];
+            for (int k=0; k<lengs2[n]; k++)
+                result[n][k]=n+k;
+        }
         return result;
     }
 }
