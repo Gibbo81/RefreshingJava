fe6a077f9529e5fd53302173125e112a6eaaa1f5
Giuliano Masi <masigiuliano@yahoo.it>
2017-09-19 20:18:10 +0200

    complete rabbit tutorial 5

---
+++ RabbitMq/src/main/java/Tutorial05/BigGreenConsumer.java
@@ -0,0 +1,31 @@
+package Tutorial05;
+
+import Tutorial01.ExtendConsumer;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class BigGreenConsumer {
+    
+    private final static String QUEUE_NAME = "Tutorial5_BigGreen_Consumer";
+    private static final String Exchange_Name = "Topic_Tutorial5";
+    private static final String Routing_Key_BigGreen = "*.big.green";
+    private final static boolean Durable = true;
+    private final static int PrefetchCount = 2;
+    
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory fac = new ConnectionFactory();
+        fac.setHost("localhost");
+        Connection con = fac.newConnection();
+        Channel cha = con.createChannel();  
+        cha.basicQos(PrefetchCount);
+        cha.queueDeclare(QUEUE_NAME, Durable, false, false, null);
+        System.out.println("Consumer will be binded for all entities big and green");
+        cha.queueBind(QUEUE_NAME, Exchange_Name, Routing_Key_BigGreen);
+        
+        System.out.println(" [Consumer] Waiting for messages.");
+        cha.basicConsume(QUEUE_NAME, new ExtendConsumer(cha));
+    }    
+}
--- RabbitMq/src/main/java/Tutorial05/PublishONTopic.java
+++ RabbitMq/src/main/java/Tutorial05/PublishONTopic.java
@@ -1,4 +1,4 @@
-package Tutorial04;
+package Tutorial05;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -7,9 +7,9 @@ import com.rabbitmq.client.MessageProperties;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class PublishOnDirectExchange {
+public class PublishONTopic {
 
-    private static final String Exchange_Name = "Direct_Tutorial4";
+    private static final String Exchange_Name = "Topic_Tutorial5";
     private static final Boolean Durable = true;
             
     public static void main(String[] args) throws IOException, TimeoutException {
@@ -17,10 +17,12 @@ public class PublishOnDirectExchange {
         fac.setHost("localhost");
         try (Connection con = fac.newConnection()){
             Channel cha = con.createChannel();  //we must close it manually because it does not implement Closeable
-            cha.exchangeDeclare(Exchange_Name, "direct",Durable);            
-            Publish(Exchange_Name, "Black","More Black Sea for all!",cha);
-            Publish(Exchange_Name,"Green","More Green Sea for all!",cha);
-            Publish(Exchange_Name,"Pink","More Pink Sea for all!",cha);
+            cha.exchangeDeclare(Exchange_Name, "topic", Durable);            
+            Publish(Exchange_Name, "animal.big.red" ,    "Our animal is: animal.big.red!",cha);
+            Publish(Exchange_Name, "animal.small.green" ,"Our animal is: animal.small.green!",cha);
+            Publish(Exchange_Name, "plant.big.green"  , "Our plant is: plant.big.green!",cha);
+            Publish(Exchange_Name, "animal.medium.green" ,"Our animal is: animal.medium.green",cha);
+            Publish(Exchange_Name, "animal.big.green" ,    "Our animal is: animal.big.green!",cha);
             cha.close();
         }
     }
